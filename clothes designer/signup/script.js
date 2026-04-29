c834c60775b1ce6309a1d2e5ec4e94fe7176c935
Omar Abozaied <omta877@gmail.com>
2026-04-29 11:34:33 +0300

    Add files via upload

---
+++ clothes designer/signup/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var password1 = document.getElementById("password1");
+  var password2 = document.getElementById("password2");
+  if (password1.type === "password") {
+    password1.type = "text";
+    password2.type = "text";
+  } else {
+    password1.type = "password";
+    password2.type = "password";
+  }
+}
+
+
